a50aa631080671ca63c387a25172b335657daa40
Jan Sambor <janeksambor@gmail.com>
2023-11-12 10:02:18 +0100

    trying some stuff

--- src/main/java/com/fd/mixin/fdMixin.java
+++ src/main/java/com/fd/mixin/fdMixin.java
@@ -2,23 +2,20 @@ package com.fd.mixin;
 
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import com.fd.Fd;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.swing.text.html.parser.Entity;
 
 @Mixin(PlayerEntity.class)
-public class fdMixin {
-    @Inject(at = @At("HEAD"), method = "updatePose")
-    private void updatePose(CallbackInfo ci) {
-        if (Fd.test.get()) {
+public abstract class fdMixin {
+    @Inject(at = @At("RETURN"), method = "updatePose")
+    private void updatePose(CallbackInfo info) {
             ((PlayerEntity) (Object) this).setPose(EntityPose.SLEEPING);
-            return;
-        }
     }
 }
