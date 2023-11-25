a0a763a6f0da643de82a67bb23e0c697727f5003
Jan Sambor <janeksambor@gmail.com>
2023-11-25 23:17:51 +0100

    not working yet, doin it tomorrow

--- src/main/java/com/fd/DevTpCommands.java
+++ src/main/java/com/fd/DevTpCommands.java
@@ -2,8 +2,11 @@ package com.fd;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import org.apache.logging.log4j.core.jmx.Server;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -12,12 +15,19 @@ import static net.minecraft.server.command.CommandManager.*;
 public class DevTpCommands implements ModInitializer {
     @Override
     public void onInitialize() {
-        // teleport the player to coords
-        for (Map.Entry<String, Map<String, ArrayList<Integer>>> entry : DevTpCoords.getCoords().entrySet()
-             ) {
-            CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("dsa")
+        // teleport the player to coords in the correct dimention
+        for (Map.Entry<String, Map<String, ArrayList<Integer>>> entry : DevTpCoords.getCoords().entrySet()) {
+            CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal(entry.getKey())
                     .executes(context -> {
-                        context.getSource().sendFeedback(() -> Text.literal("Teleported to %s".formatted("test")), false);
+                        if (context.getSource().getPlayer() == null) return 0;
+                        // teleport the player to coords in dimention
+//                        context.getSource().getPlayer().teleport();
+                        if (entry.getValue().containsKey("nether")) {
+                            // get nether world
+//                            ServerWorld.NETHER.getRegistry()
+                            context.getSource().getPlayer().getServerWorld().getDimension();
+                        }
+                        context.getSource().sendFeedback(() -> Text.literal("Teleported to %s".formatted("burning village")), false);
                         return 1;
                     })));
         }
